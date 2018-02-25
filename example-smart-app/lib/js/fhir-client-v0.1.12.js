41b380b69e1763d0ed67f8af822cb7fb0f24be80
hidhingra31 <36542988+hidhingra31@users.noreply.github.com>
2018-02-25 18:26:07 -0500

    Update fhir-client-v0.1.12.js

--- example-smart-app/lib/js/fhir-client-v0.1.12.js
+++ example-smart-app/lib/js/fhir-client-v0.1.12.js
@@ -16522,7 +16522,7 @@ SignStream.prototype.sign = function sign() {
 };
 
 	
-function isIdTokenValid(){	
+function isIdTokenValid(id_token){	
   var url = 'https://authorization.sandboxcerner.com/tenants/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/oidc/idsps/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/.well-known/openid-configuration';
   Adapter.get().http({
         type: 'GET',
